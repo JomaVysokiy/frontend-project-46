2fc2e165573d056687f8193d924528db1f178b95
Артем Нигматуллин <artem-nigmatullin@bk.ru>
2023-10-07 20:50:16 +0400

    changed src/index.js .eslintrc.yml bin/gendiff.js src/index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
-import { Command } from "commander";
-import gendiff from "../src/index.js";
-import { getFixturePath, getContentFile, getObject } from "../src/commander.js";
+import { Command } from 'commander';
+import gendiff from '../src/index.js';
+import { getFixturePath, getContentFile, getObject } from '../src/commander.js';
+
 const program = new Command();
 
 program
-  .name("gendiff")
-  .description("Compares two configuration files and shows a difference.")
-  .version("0.8.0")
-  .option("-f, --format <type>", "output format")
-  .arguments("<filepath1> <filepath2>")
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     const firstPath = getFixturePath(filepath1);
     const secondPath = getFixturePath(filepath2);
@@ -17,8 +18,7 @@ program
     const strSecondFile = getContentFile(secondPath);
     const objContentFirstFile = getObject(strFirstFile);
     const objContentSecondFile = getObject(strSecondFile);
-
     console.log(gendiff(objContentFirstFile, objContentSecondFile));
-  });
+});
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
 import _ from 'lodash';
-// const _ = require('lodash');
 
 export default (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
   const keys = [...keys1, ...keys2].sort();
 
-  let str = '';
-  let arr = [];
-
-  for (const key of keys) {
-    if (!Object.hasOwn(data1, key)) {
+  let list = keys.map((key) => {
+    let str = '';
+    if (!_.has(data1, key)) {
       // 'added'
-      str = `${str} + ${key}: ${data2[key]}, \n`;
-    } else if (!Object.hasOwn(data2, key)) {
+      str = `${str} + ${key}: ${data2[key]}`;
+    } else if (!_.has(data2, key)) {
       // 'deleted'
-      str = `${str} - ${key}: ${data1[key]}, \n`;
+      str = `${str} - ${key}: ${data1[key]}`;
     } else if (data1[key] !== data2[key]) {
       // 'changed'
-      str = `${str} - ${key}: ${data1[key]}, \n`;
-      str = `${str} + ${key}: ${data2[key]}, \n`;
+      str = `${str} - ${key}: ${data1[key]}\n`;
+      str = `${str} + ${key}: ${data2[key]}`;
     } else {
       // 'unchanged'
-      str = `${str}   ${key}: ${data1[key]}, \n`;
+      str = `${str}   ${key}: ${data1[key]}`;
     }
-    arr = str.split('\n');
+    let arr = str.split('\n');
     arr = _.uniq(arr);
     str = arr.join('\n');
-  }
-  return `{\n${str}}`;
+    return str;
+  });
+
+  list = _.uniq(list);
+  const result = list.join('\n');
+  return `{\n${result}\n}`;
 };
