443dcafab441b4e53f663d9ea13ae224f63c40cd
Артем Нигматуллин <artem-nigmatullin@bk.ru>
2023-10-08 12:53:28 +0400

    changed bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,19 +6,19 @@ import { getFixturePath, getContentFile, getObject } from '../src/commander.js';
 const program = new Command();
 
 program
-	.name('gendiff')
-	.description('Compares two configuration files and shows a difference.')
-	.version('0.8.0')
-	.option('-f, --format <type>', 'output format')
-	.arguments('<filepath1> <filepath2>')
-	.action((filepath1, filepath2) => {
-		const firstPath = getFixturePath(filepath1);
-		const secondPath = getFixturePath(filepath2);
-		const strFirstFile = getContentFile(firstPath);
-		const strSecondFile = getContentFile(secondPath);
-		const objContentFirstFile = getObject(strFirstFile);
-		const objContentSecondFile = getObject(strSecondFile);
-		console.log(gendiff(objContentFirstFile, objContentSecondFile));
-	});
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const firstPath = getFixturePath(filepath1);
+    const secondPath = getFixturePath(filepath2);
+    const strFirstFile = getContentFile(firstPath);
+    const strSecondFile = getContentFile(secondPath);
+    const objContentFirstFile = getObject(strFirstFile);
+    const objContentSecondFile = getObject(strSecondFile);
+    console.log(gendiff(objContentFirstFile, objContentSecondFile));
+  });
 
 program.parse();
